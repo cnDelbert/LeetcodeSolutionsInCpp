c766051425a6e6f2dd12c8c89ad6717385b7e9dd
Delbert <code@delbert.me>
2020-03-01 00:57:43 +0800

    first commit

---
+++ Solution.h
@@ -0,0 +1,18 @@
+//
+// Created by Delbert on 2020/3/1.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_SOLUTION_H
+#define LEETCODESOLUTIONSINCPP_SOLUTION_H
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    Solution();
+    ~Solution();
+    vector<int> twoSum(vector<int>& nums, int target);  // 1. two_sum
+};
+#endif //LEETCODESOLUTIONSINCPP_SOLUTION_H
